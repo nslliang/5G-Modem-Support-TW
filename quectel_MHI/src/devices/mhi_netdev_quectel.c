e0a88ccb1278347dd998bbb03add9b34b22761b4
nslliang <nslliang119@gmail.com>
2025-10-22 14:07:21 +0800

    backup

--- quectel_MHI/src/devices/mhi_netdev_quectel.c
+++ quectel_MHI/src/devices/mhi_netdev_quectel.c
@@ -9,7 +9,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  */
-#include <linux/string.h>
+
 #include <linux/module.h>
 #include <linux/version.h>
 #include <linux/kernel.h>
